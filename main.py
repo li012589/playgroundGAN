3acd858d0ee11818142ea3375211efa4fba1c8bc
Li SH <li012589li@outlook.com>
2017-07-12 22:43:57 +0800

    add print & save

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ PRE_TRAIN_D = 3#300
 TRIAN_TIMES = 10#1000000
 BATCH_SIZE = 50
 
-TF_SAVE_DIR = './TF_SAVE_DIR'
-PIC_SAVE_DIR = './PIC_SAVE_DIR'
+TF_SAVE_DIR = './TF_SAVE_DIR/'
+PIC_SAVE_DIR = './PIC_SAVE_DIR/'
+SAVE_PER_STEP = 2
 
 def main():
     mnist = input_data.read_data_sets("MNIST/")
@@ -23,13 +24,36 @@ def main():
     gan = GAN(sess,IMG_SIZE,Z_SIZE,G_LEARNING_RATE,D_LEARNING_RATE)
     gan.init()
 
+    saver = tf.train.Saver()
+    checkpoint = tf.train.get_checkpoint_state(TF_SAVE_DIR)
+    if checkpoint and checkpoint.model_checkpoint_path:
+        saver.restore(sess, checkpoint.model_checkpoint_path)
+        print("Successfully loaded:", checkpoint.model_checkpoint_path)
+    else:
+        print("Could not find old network weights")
+
+    t = 0
+    state = "pre-train"
     for i in xrange(PRE_TRAIN_D):
+        t+=1
         realDate = mnist.train.next_batch(BATCH_SIZE)[0].reshape([BATCH_SIZE,IMG_SIZE[0],IMG_SIZE[1],IMG_SIZE[2]])
         gan.trainD(realDate,BATCH_SIZE)
 
+        print("TimeStep",t,"/ State",state)
+        if t % SAVE_PER_STEP == 0:
+            saver.save(sess, TF_SAVE_DIR + '-gan', global_step = t)
+            print("Saved")
+
+    state = 'train'
     for i in xrange(TRIAN_TIMES):
+        t+=1
         realDate = mnist.train.next_batch(BATCH_SIZE)[0].reshape([BATCH_SIZE,IMG_SIZE[0],IMG_SIZE[1],IMG_SIZE[2]])
         gan.trainG(BATCH_SIZE)
+
+        print("TimeStep",t,"/ State",state)
+        if t % SAVE_PER_STEP == 0:
+            saver.save(sess, TF_SAVE_DIR + '-gan', global_step = t)
+            print("Saved")
 
 if __name__ == "__main__":
     main()
